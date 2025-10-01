bfd847f8d64633b1b77e05248ebff23f75d15491
Joseph Kawe <kohako.tech@gmail.com>
2025-10-01 12:14:08 -0300

    feat: add data from use on site -Data Link -Paintings to use on Swipper -Data with paitings infos

---
+++ public/data/paintings.ts
@@ -0,0 +1,28 @@
+import { SwiperPainting } from "@/types/types";
+
+export const paintings: SwiperPainting[] = [
+  {
+    src: "/assets/Paintings/PAINT_noite_estrelada.jpg",
+    alt: "A Noite Estrelada, de Vincent van Gogh",
+  },
+  {
+    src: "/assets/Paintings/PAINT_noite_estrelada_rodano.jpg",
+    alt: "Noite Estrelada Sobre o Ródano, de Vincent van Gogh",
+  },
+  {
+    src: "/assets/Paintings/PAINT_cafe_noite.jpg",
+    alt: "O Terraço do Café à Noite, de Vincent van Gogh",
+  },
+  {
+    src: "/assets/Paintings/PAINT_noite_estrelada.jpg",
+    alt: "A Noite Estrelada, de Vincent van Gogh",
+  },
+  {
+    src: "/assets/Paintings/PAINT_noite_estrelada_rodano.jpg",
+    alt: "Noite Estrelada Sobre o Ródano, de Vincent van Gogh",
+  },
+  {
+    src: "/assets/Paintings/PAINT_cafe_noite.jpg",
+    alt: "O Terraço do Café à Noite, de Vincent van Gogh",
+  },
+];
