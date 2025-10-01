5a45e02c8c188bcfa4f0f424990dd93f0d30cff6
Joseph Kawe <kohako.tech@gmail.com>
2025-10-01 12:16:00 -0300

    feat: create and improve main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,109 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { PaintingCarousel } from "@/components/PaintingCarousel";
+import { paintings } from "../../public/data/paintings";
+import { AppSidebar } from "@/components/AppSidebar";
+import Link from "next/link";
+import logo from "../../public/assets/logo.png";
+import { motion } from "motion/react";
+import { SparklesCore } from "@/components/ui/sparkles";
+import { useTheme } from "next-themes";
+
+export default function HomePage() {
+  const [open, setOpen] = useState(false);
+  const theme = useTheme().theme;
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="flex flex-col min-h-screen font-josefin">
+      <div
+        className="absolute inset-0 w-full h-full pointer-events-none"
+        aria-hidden="true"
+      >
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={25}
+          className="w-full h-full"
+          particleColor={theme === "dark" ? "#d4d4d8" : "#09090b"}
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      </div>
+      <AppSidebar open={open} setOpen={setOpen} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <main
+        className="flex flex-col items-center justify-center flex-1 w-full max-w-7xl px-4 mx-auto text-center"
+        role="main"
+      >
+        <header className="mb-4 mt-10 lg:mt-0">
+          <motion.h1
+            initial={{ opacity: 0, x: 30 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className="flex flex-col text-5xl font-bold font-josefin sm:text-6xl"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Van Gogh{" "}
+            <motion.span
+              initial={{ opacity: 0, x: -60 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 0 }}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              className="text-6xl sm:text-7xl font-brush bg-gradient-to-br from-blue-700 via-blue-500 to-cyan-700 text-transparent bg-clip-text -mt-10"
+            >
+              Univers
+            </motion.span>
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: 80 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className="text-lg z-30 leading-relaxed font-josefin max-w-2xl mx-auto"
           >
-            Read our docs
-          </a>
-        </div>
+            Explore a genialidade e a emoção de um dos maiores mestres da arte
+            em uma galeria digital interativa e acessível.
+          </motion.p>
+        </header>
+
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
+          aria-label="Galeria digital de obras de Van Gogh"
+          className="w-full z-40 mb-4"
+        >
+          <PaintingCarousel paintings={paintings} />
+        </motion.section>
+
+        <motion.section
+          initial={{ opacity: 0, y: -80 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 0 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="mb-12"
+        >
+          <p className="text-lg font-josefin max-w-2xl mx-auto leading-relaxed z-30">
+            Arte, história e inspiração em cada pincelada. <br />
+            Comece agora sua jornada pelo universo de Van Gogh.
+          </p>
+
+          <div className="flex justify-center gap-3 mt-6">
+            <Link href="/paintings" passHref>
+              <Button
+                size="lg"
+                className="font-josefin"
+                aria-label="Explorar obras de Van Gogh"
+              >
+                Explorar Obras
+              </Button>
+            </Link>
+          </div>
+        </motion.section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
