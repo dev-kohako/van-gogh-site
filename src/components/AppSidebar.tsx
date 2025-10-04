d5c16a9ef9360195b22598a417f65bb8431268fa
Joseph Kawe <kohako.tech@gmail.com>
2025-10-03 21:57:44 -0300

    feat: improvement with biome

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,62 +1,62 @@
 "use client";
 
-import Image from "next/image";
-import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
-import { ModeToggle } from "@/components/toggleDarkMode";
-import { links } from "../../public/data/links";
-import logo from "../../public/assets/logo.png";
 import { motion } from "motion/react";
+import Image from "next/image";
 import Link from "next/link";
-import { AppSidebarProps } from "@/types/types";
+import { useState } from "react";
+import { ModeToggle } from "@/components/toggleDarkMode";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import logo from "../../public/assets/logo.png";
+import { links } from "../../public/data/links";
 
-export function AppSidebar({ open, setOpen }: AppSidebarProps) {
+export function AppSidebar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <Sidebar open={open} setOpen={setOpen}>
-      <SidebarBody
-        role="navigation"
-        aria-label="Menu lateral"
-        className="absolute flex flex-col justify-between w-full min-h-screen gap-10 border-r bg-background dark:bg-background"
-      >
-        <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-          <Link
-            href="/"
-            aria-label="Página inicial da KWK Tech"
-            className="relative z-20 flex items-center space-x-2 py-2"
-          >
-            <Image
-              src={logo}
-              width={24}
-              height={24}
-              priority
-              alt="Logo da KWK Tech"
-              className="object-contain invert dark:invert-0"
-            />
-            <motion.span
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              className="pt-1 text-base font-medium whitespace-pre text-foreground"
+      <SidebarBody className="justify-between gap-10 fixed w-full">
+        <div className="w-full flex flex-col flex-1 justify-between overflow-y-auto overflow-x-hidden">
+          <section>
+            <Link
+              href="/"
+              aria-label="Página inicial da KWK Tech"
+              className="relative z-20 flex items-center space-x-2 py-2"
             >
-              KWK Tech
-            </motion.span>
-          </Link>
+              <Image
+                src={logo}
+                width={24}
+                height={24}
+                priority
+                alt="Logo da KWK Tech"
+                className="object-contain invert dark:invert-0"
+              />
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="pt-1 text-base font-medium whitespace-pre text-foreground"
+              >
+                KWK Tech
+              </motion.span>
+            </Link>
 
-          <nav
-            className="mt-10 flex flex-col gap-2"
-            aria-label="Links principais"
-          >
-            {links.map((link, idx) => (
-              <SidebarLink key={idx} link={link} />
-            ))}
-          </nav>
-        </div>
-
-        <div className="flex flex-col items-start gap-2">
-          <SidebarLink
-            link={{
-              label: `© ${new Date().getFullYear()} KWK Tech`,
-              icon: <ModeToggle />,
-            }}
-          />
+            <nav
+              className="mt-10 flex flex-col gap-2"
+              aria-label="Links principais"
+            >
+              {links.map((link) => (
+                <SidebarLink key={crypto.randomUUID()} link={link} />
+              ))}
+            </nav>
+          </section>
+          <div className="flex flex-col items-start gap-2 relative">
+            <SidebarLink
+              link={{
+                label: `© ${new Date().getFullYear()} KWK Tech`,
+                icon: <ModeToggle />,
+                href: "#",
+              }}
+            />
+          </div>
         </div>
       </SidebarBody>
     </Sidebar>
