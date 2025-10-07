14cb464626412da89a2332f4bb5bd5a88c33b792
Joseph Kawe <kohako.tech@gmail.com>
2025-10-06 23:02:10 -0300

    chore: change image src

--- public/data/paintings.ts
+++ public/data/paintings.ts
@@ -2,27 +2,27 @@ import type { SwiperPainting } from "@/types/types";
 
 export const paintings: SwiperPainting[] = [
   {
-    src: "/assets/Paintings/PAINT_noite_estrelada.jpg",
+    src: "/assets/paintings/PAINT_noite_estrelada.jpg",
     alt: "A Noite Estrelada, de Vincent van Gogh",
   },
   {
-    src: "/assets/Paintings/PAINT_noite_estrelada_rodano.jpg",
+    src: "/assets/paintings/PAINT_noite_estrelada_rodano.jpg",
     alt: "Noite Estrelada Sobre o Ródano, de Vincent van Gogh",
   },
   {
-    src: "/assets/Paintings/PAINT_cafe_noite.jpg",
+    src: "/assets/paintings/PAINT_cafe_noite.jpg",
     alt: "O Terraço do Café à Noite, de Vincent van Gogh",
   },
   {
-    src: "/assets/Paintings/PAINT_noite_estrelada.jpg",
+    src: "/assets/paintings/PAINT_noite_estrelada.jpg",
     alt: "A Noite Estrelada, de Vincent van Gogh",
   },
   {
-    src: "/assets/Paintings/PAINT_noite_estrelada_rodano.jpg",
+    src: "/assets/paintings/PAINT_noite_estrelada_rodano.jpg",
     alt: "Noite Estrelada Sobre o Ródano, de Vincent van Gogh",
   },
   {
-    src: "/assets/Paintings/PAINT_cafe_noite.jpg",
+    src: "/assets/paintings/PAINT_cafe_noite.jpg",
     alt: "O Terraço do Café à Noite, de Vincent van Gogh",
   },
 ];
