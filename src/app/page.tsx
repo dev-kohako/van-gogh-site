953a0d166f37540eae5bd6a0d1d6400478d49f95
Joseph Kawe <kohako.tech@gmail.com>
2025-10-10 05:19:25 -0300

    chore(mainPage) adjust pl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { HomePageClient } from "./home-page-client";
 export default function HomePage() {
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen font-josefin pt-5 sm:pt-10 md:pt-0 relative">
+    <div className="flex flex-col justify-center items-center min-h-screen font-josefin pt-5 sm:pt-10 md:pt-0 relative md:pl-10 lg:!pl-0">
       <main className="flex flex-col items-center justify-center flex-1 w-full max-w-7xl px-4 mx-auto text-center">
         <HomePageClient paintings={paintings} />
       </main>
